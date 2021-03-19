588de3403ac51d66968d214454aad0a7d5632cfe
Arkadiusz Michalski <nospam@please.com>
2021-03-19 22:25:39 +0100

    Update 1

--- README.md
+++ README.md
@@ -1,2 +1,65 @@
 # NPP_plugins_test
-Tool for testing all plugins for Notepad ++ against crash
+Tool for downloading and testing all plugins for Notepad++ against crash. Due to better portability (e.g. relative to Python, especially for Windows 7) I wrote it in NodeJS. Everything works locally after following the steps below.
+
+## Attention!
+
+Testing all plugins may cause the following problems:
+- Files are not analyzed by any antivirus service (so they can be potentially dangerous). If you have a real-time antivirus installed, it will be probably checked them automatically. You can also download all .zip filses first, check them manually by your antivirus, and after that run the test again.
+- Some plugins may change system settings, like `NotepadStarterPlugin`. They probably have some `.bat` files to uninstall, but I haven't checked all of them.
+
+For the above reasons, it's best to use this tool in a virtual machine. **I'm not responsible for any damage done due to testing**. If you don't know what you are doing, it's best not to use this tool at all.
+
+
+### How use it
+
+1. Download some portable version of Notepad++ from the [official page](https://notepad-plus-plus.org/downloads/) (`.zip` or `.7z` package). You can also try some newer versions ([details](https://github.com/notepad-plus-plus/notepad-plus-plus/wiki/Testing)).
+2. Extract the above package to some folder (for example `NPP`).
+3. Download the `_test` folder from this repo and put it to above `NPP` folder.
+4. Inside `_test` folder put `pl.x86.json` or `pl.x64.json` file depending on which version you downloaded in step 1. You can download the content of this file from the [official nppPluginList](https://github.com/notepad-plus-plus/nppPluginList) repo. Check the latest version of [pl.x86.json](https://github.com/notepad-plus-plus/nppPluginList/blob/master/src/pl.x86.json) or [pl.x64.json](https://github.com/notepad-plus-plus/nppPluginList/blob/master/src/pl.x64.json) file.
+5. Start tool with `run.bat` file (or `run_win7.bat` for Windows 7).
+
+You can test different versions of NPP and different versions of the plugin list, to locate the plugins that break the NPP version you are testing.
+
+### Configuration
+
+At the beginning of the `_test\plugins.js` file we have some configuration (you can change them and run the test again):
+```
+// CONFIGURATION START
+
+var nppSync = false; //true - run Notepad++ in sync mode (need manually close all popup), false - run Notepad++ in async mode (all popup will be close automatically)
+var closePopupTime = 2500; // in async mode wait the given number of milliseconds before closing any popup automatically
+var closeNppTime = 10000; //  in async mode wait the given number of milliseconds before closing Notepad++ main window automatically
+
+var plugListFile_x86 = "pl.x86.json"; // 32-bit plugin list file
+var plugListFile_x64 = "pl.x64.json"; // 66-bit plugin list file
+
+var onlyDownload = false; // true - only downloads .zip archives without test it under Notepad++, false - downloads .zip archives and test it under Notepad++
+var retryDownload = 3; // 0 - try download .zip archive only once , >0 will try the specified number of times
+var ignorePlugins = [ // ignore plugin list (put "folder-name" of any plugin to ignore it, like ["Plugin1", "Plugin2", "Plugin3"])
+
+];
+
+// CONFIGURATION END
+```
+
+### Additional tips
+- If you already have NodeJS installed on your system, you can delete the `_test\node.exe` file.
+- The downloaded `.zip` packages are saved in the `_test\_cache` folder. If you are running the test again and want them to be downloaded again every time, then clean this folder before you run test again.
+- Plugins save their settings in a `NPP\plugins\Config`folder. If you are running the test again and want it to be not use again, then clean this folder before you run test again.
+- You can copy the entire `_test` folder (with or without `_cache` folder) to another version of NPP and test it with the same or a different plugin list.
+- After downloading all `.zip` packages, you can copy the entire contents of `_cache` folder to another place to have access to this files in the future.
+- You can list a small number of plugins in the plugin list file to test this tool with a smaller amount of data. This way you can, for example, test only plugins that you trust.
+- You can manually generate a crash by copying to the `NPP\plugins` folder some known plugin version that causes problem (or put information about it to plugin list file).
+
+### Dependencies
+- NodeJS (included 15.4.0) and some packages inside `_test\node_modules`:
+	- adm-zip 0.5.4
+	- filendir 2.0.0
+	- follow-redirects 1.13.3
+- [NirCMD v2.86 (64-bit)](http://www.nirsoft.net/utils/nircmd.html) inside `_test\_winsoft\NirCmd64`
+
+### TODO
+- Check hash after downloading the `.zp` file.
+- Find an easier way to close NPP when the popup appears (without using any additional programs).
+- Add configuration also through CLI arguments.
+
---
+++ plugins.js
@@ -0,0 +1,448 @@
+var version = "2021.03.19.01";
+
+// CONFIGURATION START
+
+var nppSync = false; //true - run Notepad++ in sync mode (need manually close all popup), false - run Notepad++ in async mode (all popup will be close automatically)
+var closePopupTime = 2500; // in async mode wait the given number of milliseconds before closing any popup automatically
+var closeNppTime = 10000; //  in async mode wait the given number of milliseconds before closing Notepad++ main window automatically
+
+var plugListFile_x86 = "pl.x86.json"; // 32-bit plugin list file
+var plugListFile_x64 = "pl.x64.json"; // 66-bit plugin list file
+
+var onlyDownload = true; // true - only downloads .zip archives without test it under Notepad++, false - downloads .zip archives and test it under Notepad++
+var retryDownload = 3; // 0 - try download .zip archive only once , >0 will try the specified number of times
+var ignorePlugins = [ // ignore plugin list (put "folder-name" of any plugin to ignore it, like ["Plugin1", "Plugin2", "Plugin3"])
+
+];
+
+// CONFIGURATION END
+
+var cwd = process.cwd();
+
+var child = require("child_process");
+var https = require("follow-redirects").https;
+var http = require("follow-redirects").http;
+var Stream = require("stream").Transform;
+var fs = require("fs");
+var fs2 = require("filendir");
+var AdmZip = require("adm-zip");
+
+var plugList;
+var plugListInfo = "";
+var plugCache = cwd + "\\_cache\\";
+
+var npp = cwd + "\\..\\";
+var nppExe = npp + "notepad++.exe";
+var nppPlug = npp + "plugins\\";
+var winsoft = cwd + "\\_winsoft\\";
+var nircmd64 = winsoft + "NirCmd64\\";
+var nircmd64Exe = nircmd64 + "nircmdc.exe";
+
+var plugins;
+var totalStep;
+var currentStep = 0;
+var pluginsOK = 0;
+var pluginsBAD = 0;
+var pluginsBADinfo = "";
+var downloadStep = 0;
+var downloadBAD = 0;
+var downloadBADinfo = "";
+var timePopupRef;
+var timeNppRef
+var totalTime;
+
+
+run();
+
+
+function run(){
+
+	if (fs.existsSync("pl.x86.json")){
+	 plugList = require(cwd + "\\" + plugListFile_x86);
+	}
+	else if (fs.existsSync("pl.x64.json")){
+	 plugList = require(cwd + "\\" + plugListFile_x64);
+	}
+
+	//console.log(plugList);
+
+	if (plugList){
+
+		totalTime = new Date() * 1;
+		analyzePluginsList();
+
+	}
+	else{
+		console.log("Need plugin list as " + plugListFile_x86 + "or " + plugListFile_x64 + " file!");
+	}
+
+}
+
+
+function analyzePluginsList(){
+
+	var ver = plugList.version;
+	var arch = plugList.arch;
+	plugins = plugList["npp-plugins"];
+	totalStep = plugins.length;
+
+	plugListInfo += "Plugin List:" + "\n";
+	plugListInfo += "version: " + ver + "\n";
+	plugListInfo += "arch: " + arch + "\n";
+	plugListInfo += "plugins: " + totalStep;
+
+	console.log(plugListInfo);
+	console.log("");
+
+	analyzePlugin(plugins[currentStep]);
+
+}
+
+
+function analyzePlugin(plugin, recurse){
+
+	var folder = plugin["folder-name"];
+	var file = folder + ".dll";
+	var ver = plugin.version;
+	var folder_ver = folder + "_" + ver;
+	var full_plugin_folder = nppPlug + folder;
+	var url = plugin.repository;
+
+	if (!recurse){
+		console.log("[" + (currentStep + 1) + "/" + totalStep + "] Start analyze: " + folder_ver);
+	}
+
+	if (ignorePlugins.includes(folder)){
+
+		console.log(folder_ver + ": " + "Ignored");
+		pluginsOK += 1;
+		currentStep += 1;
+		return isFinish();
+
+	}
+
+	var zipExists = fs.existsSync(plugCache + folder_ver + ".zip");
+
+	if (zipExists && !recurse){
+		console.log(folder_ver + ": " + "Cached");
+	}
+
+	if (zipExists && onlyDownload){
+
+		pluginsOK += 1;
+		currentStep += 1;
+		return isFinish();
+
+	}
+
+	else if (zipExists && !onlyDownload){
+
+		if (fs.existsSync(full_plugin_folder)){
+			fs.rmdirSync(full_plugin_folder, {"recursive": true});
+		}
+
+		var zip = new AdmZip(plugCache + folder_ver + ".zip");
+		zip.extractAllTo(full_plugin_folder, true); // overwrite
+
+		if (nppSync){
+			runNPPsync(folder_ver, full_plugin_folder);
+		}
+		else{
+			runNPPasync(folder_ver, full_plugin_folder);
+		}
+
+	}
+	else{
+
+		downloadStep = 0;
+		return getPluginZip(url, plugCache, folder_ver, plugin);
+
+	}
+
+/*
+	if (fs.existsSync(nppPlug + folder + "\\" + file)){
+
+		console.log(folder_ver);
+
+		if (getFileVersion(nppPlug + folder + "\\" + file) == ver){
+			console.log(folder_ver)
+		}
+		else{ // usuń jeśli wersja się nie zgadza
+
+			// UWAGA: niestety wersja w liście bardzo często nie jest kompletna i nie zawiera all cyfr, które są widoczne we właściwościach pliku, ehh...
+			// Na razie to trzeba pominąć, trzeba po prostu zawsze usuwać istniejący folder i wypakowywać z cache albo ściągać!!!
+
+		}
+
+
+
+
+	}
+	else if (fs.existsSync(nppPlug + "_" + folder)){
+
+	}
+	else if (fs.existsSync(plugCache + folder_ver + ".zip")){
+		//runNPP(folder_ver);
+	}
+	*/
+
+
+
+	//fs.existsSync() // https://nodejs.org/api/fs.html#fs_fs_existssync_path
+
+	//fs.copyFileSync() // https://nodejs.org/api/fs.html#fs_fs_copyfilesync_src_dest_mode
+
+	//fs.renameSync(oldPath, newPath) // https://nodejs.org/api/fs.html#fs_fs_renamesync_oldpath_newpath
+
+	// fs.rmSync(path[, options]) // https://nodejs.org/api/fs.html#fs_fs_rmsync_path_options
+
+}
+
+
+function getPluginZip(url, plugCache, folder_ver, plugin){
+
+	var path = plugCache + folder_ver + ".zip";
+
+	var correctTool = https;
+	if (url.match(/^http:/)){
+		correctTool = http;
+	}
+
+	console.log("Start downloading: " + url);
+
+	correctTool.get(url, function(res){
+
+		if (res.statusCode != 200){
+
+			console.log("Download error: " + res.statusCode);
+
+			if (retryDownload > 0 && downloadStep < retryDownload - 1){
+
+				res.resume();
+				downloadStep += 1;
+				return getPluginZip(url, plugCache, folder_ver, plugin);
+
+			}
+
+			res.resume();
+			currentStep += 1;
+			downloadBAD += 1;
+			downloadBADinfo += "\n" + folder_ver + ": " + url;
+
+			return isFinish();
+
+		}
+
+		var data = new Stream();
+
+		res.on("data", function(chunk){
+			data.push(chunk);
+		});
+
+		res.on("end", function(){
+
+			console.log("Download finished: OK");
+
+			fs2.writeFileSync(path, data.read());
+			analyzePlugin(plugin, true);
+
+		});
+
+	});
+
+}
+
+
+function getFileVersion(path){
+
+	var exist = false;
+
+	var checkProcess = child.execSync("wmic datafile where name='" + path.replace(/\\/g, "\\\\") + "' " + "get Version");
+	var data = checkProcess.toString();
+	var version = data.split("Version")[1].trim();
+	//console.log(data);
+	//console.log(version);
+
+	return version;
+
+	//wmic datafile where name='c:\\windows\\system32\\notepad.exe' get Version
+	//wmic datafile where name='C:\\Users\\Arek\\Desktop\\NPP\\_test\\..\\plugins\\mimeTools\\mimeTools.dll' get Version // works
+}
+
+
+function runNPPasync(folder_ver, full_plugin_folder){
+
+	var nppProcess = child.execFile(nppExe, ["-multiInst", "-export=functionList"], function(error, stdout, stderr){
+
+		//console.log(stderr); // string
+		//console.log("stdout: " + stdout); // string
+		//console.log("error: " + error); // null when all jest OK (otherwise the object )
+		//console.log(nppProcess.signalCode);
+
+		if (!error){
+
+			//console.log("Close manually!"); // close NPP in standard way
+			pluginsOK += 1;
+			console.log(folder_ver + ": " + "OK");
+
+		}
+		else{
+
+			if (nppProcess.signalCode == "SIGTERM"){ // close NPP through nppProcess.kill();
+
+				//console.log("Close programmatically!");
+				pluginsOK += 1;
+				console.log(folder_ver + ": " + "OK");
+
+			}
+			else{
+
+				//console.log("Close due of crash!"); // was crash
+				pluginsBAD += 1;
+				pluginsBADinfo += "\n" + folder_ver;
+				console.log(folder_ver + ": " + "BAD");
+
+			}
+
+		}
+
+		clearTimeout(timePopupRef);
+		clearTimeout(timeNppRef);
+
+		if (folder_ver.indexOf("NotepadStarterPlugin") != -1){
+
+			try{
+				child.execSync(full_plugin_folder + "\\NotepadStarter\\NotepadStarterUninstall.bat");
+			}
+			catch(e){}
+
+		}
+
+		console.log(folder_ver + ": " + "Delete");
+		fs.rmdirSync(full_plugin_folder, {"recursive": true});
+
+		currentStep += 1;
+		isFinish();
+
+	});
+
+
+	timePopupRef = setTimeout(function(){
+
+		child.execFile(nircmd64Exe, ["dlg", nppExe, "", "click", "no"]);
+		child.execFile(nircmd64Exe, ["dlg", nppExe, "", "click", "cancel"]);
+
+		// Dialog with problem:
+		// iTimeTrack << can't close with above commands
+		// FingerText << without Config/FingerText folder open new tab, so trying close Npp window will ask to save file, click "no" before "cancel" solves such problem
+		// GedcomLexer << display second popup after close first (Yes/No)
+
+		if (folder_ver.indexOf("iTimeTrack") != -1){
+			child.execFile(nircmd64Exe, ["win", "close", "title", "iTimeTrack"]);
+		}
+
+		if (folder_ver.indexOf("GedcomLexer") != -1){ // after first close display another dialog
+
+			timePopupRef = setTimeout(function(){
+
+				child.execFile(nircmd64Exe, ["dlg", nppExe, "", "click", "no"]);
+				child.execFile(nircmd64Exe, ["dlg", nppExe, "", "click", "cancel"]);
+
+			}, closePopupTime);
+
+		}
+
+	}, closePopupTime);
+
+
+	timeNppRef = setTimeout(function(){
+
+		nppProcess.kill(); // in nppProcess.signalCode will be SIGTERM
+
+	}, closeNppTime);
+
+}
+
+
+function runNPPsync(folder_ver, full_plugin_folder){
+
+	var nppProcess;
+
+	try {
+
+		nppProcess = child.execFileSync(nppExe, ["-multiInst", "-export=functionList"]);
+		//console.log(nppProcess.toString()); // 0 when all OK
+
+	}
+	catch(e){
+		//console.log(e);
+	}
+
+	if (nppProcess){
+
+		pluginsOK += 1;
+		console.log(folder_ver + ": " + "OK");
+
+	}
+	else{
+
+		pluginsBAD += 1;
+		console.log(folder_ver + ": " + "BAD");
+
+	}
+
+	if (folder_ver.indexOf("NotepadStarterPlugin") != -1){
+
+		try{
+			child.execSync(full_plugin_folder + "\\NotepadStarter\\NotepadStarterUninstall.bat");
+		}
+		catch(e){}
+
+	}
+
+	console.log(folder_ver + ": " + "Delete");
+	fs.rmdirSync(full_plugin_folder, {"recursive": true});
+
+	currentStep += 1;
+	isFinish();
+
+}
+
+
+function isFinish(){
+
+	if (currentStep >= totalStep){ // totalStep
+
+		var endTime = parseInt((new Date * 1 - totalTime) / 1000);
+		var minutes = parseInt(endTime / 60);
+		var seconds = endTime % 60;
+
+		console.log("");
+		console.log("");
+
+		console.log("[" + currentStep + "/" + totalStep + "] End analyze:");
+		console.log("");
+		console.log(plugListInfo);
+		console.log("");
+
+		console.log("totalTime: " + minutes + " [min] " + seconds + " [s]");
+		console.log("");
+		console.log("pluginsOK: " + pluginsOK);
+		console.log("");
+		console.log("pluginsBAD: " + pluginsBAD);
+		console.log("pluginsBADinfo: " + pluginsBADinfo);
+		console.log("");
+		console.log("downloadBAD: " + downloadBAD);
+		console.log("downloadBADinfo: " + downloadBADinfo);
+
+		// TODO return some code
+
+	}
+	else{
+
+		console.log("");
+		analyzePlugin(plugins[currentStep]);
+
+	}
+
+}
